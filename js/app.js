b643fc4c8ee32beed01464d2c3cea2ab3014f924
Florencia Amendola <amendolaflorencia@gmail.com>
2021-11-19 00:24:20 -0300

    fix shopping cart

--- js/app.js
+++ js/app.js
@@ -1,90 +1,85 @@
-$(document).ready(function(){
+/// Funcion para cargar el slider una vez que el documento ya esta en el DOM
+$(document).ready(function () {
   $(".owl-carousel").owlCarousel();
-
-});
-if (document.getElementById('hearderSlide')) {
-	$('#hearderSlide').multislider();
-	$('#hearderSlide').multislider('pause');
-}
-
-
-function closeCart() {
-	const cart = document.querySelector('.producstOnCart');
-	cart.classList.toggle('hide');
-	document.querySelector('body').classList.toggle('stopScrolling')
-}
-
-
-const openShopCart = document.querySelector('.shoppingCartButton');
-openShopCart.addEventListener('click', () => {
-	const cart = document.querySelector('.producstOnCart');
-	cart.classList.toggle('hide');
-	document.querySelector('body').classList.toggle('stopScrolling');
 });
 
+///Funcion para el toggle del carrito esquina
+const closeCart = function () {
+  const cart = document.querySelector(".producstOnCart");
+  cart.classList.toggle("hide");
+  document.querySelector("body").classList.toggle("stopScrolling");
+};
 
-const closeShopCart = document.querySelector('#closeButton');
-const overlay = document.querySelector('.overlay');
-closeShopCart.addEventListener('click', closeCart);
-overlay.addEventListener('click', closeCart);
+/// Abriendo el carito al hacer click en icono carrito
+const openShopCart = document.querySelector(".shoppingCartButton");
+openShopCart.addEventListener("click", () => {
+  const cart = document.querySelector(".producstOnCart");
+  cart.classList.toggle("hide");
+  document.querySelector("body").classList.toggle("stopScrolling");
+});
 
+/// Cerrando carrito al hacer click en la cruz
+const closeShopCart = document.querySelector("#closeButton");
+const overlay = document.querySelector(".overlay");
+closeShopCart.addEventListener("click", closeCart);
+overlay.addEventListener("click", closeCart);
 
- /// RECORRO EL ARRAY DE TODOS LOS PRODUCTOS
- for (let bicicleta of todasLasBicicletas) {
-  crearFichaProducto(bicicleta);
- 
-}
-console.log(todasLasBicicletas)
-
-/// FUNCION PARA RENDERIZAR FICHAS DE CADA PRODUCTO POR MEDIO DEL DOM
-
-function crearFichaProducto(bici) {
+/// Funcion para renderizar las cards de cada producto
+const crearFichaProducto = function (bici) {
   let cont_father = document.getElementById("contenidoGenerado");
   let cont_child = document.createElement("div");
   cont_child.innerHTML = `<div class="container">
-  <div class="product" style="width:20rem">
-                        <div class="product-under">
-                            <div class="product-image">
-                                <img src=${bici.image} />
+                            <div class="product" style="width:20rem">
+                                <div class="product-under">
+                                    <div class="product-image">
+                                    <img src=${bici.image} />
                                 <div class="product-over">
-                                    <button class="btn btn-small addToCart" data-product-id=${bici.id}>
+                                    <button class="btn btn-small addToCart" id=${bici.id}>
                                         <i class="fas fa-cart-plus"></i>Agregar
                                         al carrito
                                     </button>
                                     <a href="detail_page.html" class="btn btn-small">Detalle</a>
                                 </div>
+                                </div>
+                                <div class="product-summary">
+                                    <h4 class="productName">${bici.name}</h4>
+                                    <span class="stars"></span>
+                                    <p>
+                                        ${bici.description}
+                                    </p>
+                                    <h6 class="price">
+                                      $<span class="priceValue"> ${bici.price}</span>
+                                    </h6>
+                                </div>
                             </div>
-                            <div class="product-summary">
-                                <h4 class="productName">${bici.name}</h4>
-                                <span class="stars"></span>
-                                <p>
-                                    ${bici.description}
-                                </p>
-                                <h6 class="price">
-                                    $<span class="priceValue"> ${bici.price}</span>
-                                </h6>
-                            </div>
-                        </div>
-			</div>
+			            </div>
                     </div>`;
 
   cont_father.appendChild(cont_child);
+  let botones = document.getElementById(bici.id);
+  console.log(botones);
+};
+
+/// recorro el array de productos para crear una fichas por cada uno de sus indices
+for (let bicicleta of todasLasBicicletas) {
+  crearFichaProducto(bicicleta);
 }
 
-/// slider
-
-var owl = $('.owl-carousel');
+/// Animaciones del slider de fotos
+const owl = $(".owl-carousel");
 owl.owlCarousel({
-    items:4,
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:2000,
-    autoplayHoverPause:true
+  items: 4,
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  autoplayHoverPause: true,
 });
-$('.play').on('click',function(){
-    owl.trigger('play.owl.autoplay',[1000])
-})
-$('.stop').on('click',function(){
-    owl.trigger('stop.owl.autoplay')
-})
+
+$(".play").on("click", function () {
+  owl.trigger("play.owl.autoplay", [1000]);
+});
+
+$(".stop").on("click", function () {
+  owl.trigger("stop.owl.autoplay");
+});
--- js/cart.js
+++ js/cart.js
@@ -1,25 +1,47 @@
-let productsInCart = JSON.parse(localStorage.getItem('shoppingCart'));
-if(!productsInCart){
-	productsInCart = [];
-}
-const parentElement = document.querySelector('#buyItems');
-const cartSumPrice = document.querySelector('#sum-prices');
-const products = document.querySelectorAll('.product-under');
- console.log(products)
-
-const countTheSumPrice = function () { // 4
-	let sum = 0;
-	productsInCart.forEach(item => {
-		sum += item.price;
-	});
-	return sum;
+/// SECCION CLASES.... AGREGAR CLASE CLIENTE
+class Bicicleta {
+  constructor(datos) {
+    this.id = datos.id;
+    this.name = datos.name;
+    this.price = datos.price;
+    this.basePrice = datos.price;
+    this.count = datos.count;
+    this.image = datos.image;
+    this.description = datos.description;
+  }
 }
 
-const updateShoppingCartHTML = function () {  // 3
-	localStorage.setItem('shoppingCart', JSON.stringify(productsInCart));
-	if (productsInCart.length > 0) {
-		let result = productsInCart.map(product => {
-			return `
+//// ORDENAR VARIABLES GLOBALES QUEE S UN QUILOMBO JAJAJ
+const listaProductos = [];
+
+for (let i = 0; i < todasLasBicicletas.length; i++) {
+  listaProductos.push(new Bicicleta(todasLasBicicletas[i]));
+}
+console.log(listaProductos);
+
+let productsInCart = JSON.parse(localStorage.getItem("shoppingCart"));
+if (!productsInCart) {
+  productsInCart = [];
+}
+const parentElement = document.querySelector("#buyItems");
+const cartSumPrice = document.querySelector("#sum-prices");
+
+/// FUNCION PARA SUMAR EL PRECIO TOTAL DE LA COMPRA VER DE PROBAR CON REDUCE
+const SumPrice = function () {
+  // 4
+  let sum = 0;
+  productsInCart.forEach((item) => {
+    sum += item.price;
+  });
+  return sum;
+};
+/// FUNCION PARA RENDERIZAR EL CARRITO
+const updateShoppingCartHTML = function () {
+  // 3
+  localStorage.setItem("shoppingCart", JSON.stringify(productsInCart));
+  if (productsInCart.length > 0) {
+    let result = productsInCart.map((product) => {
+      return `
 				<li class="buyItem">
 					<img src="${product.image}">
 					<div>
@@ -31,73 +53,66 @@ const updateShoppingCartHTML = function () {  // 3
 							<button class="button-plus" data-id=${product.id}>+</button>
 						</div>
 					</div>
-				</li>`
-		});
-		parentElement.innerHTML = result.join('');
-		document.querySelector('.checkout').classList.remove('hidden');
-		cartSumPrice.innerHTML = '$' + countTheSumPrice();
-
-	}
-	else {
-		document.querySelector('.checkout').classList.add('hidden');
-		parentElement.innerHTML = '<h4 class="empty">Your shopping cart is empty</h4>';
-		cartSumPrice.innerHTML = '';
-	}
+				</li>`;
+    });
+    parentElement.innerHTML = result.join("");
+    //document.querySelector('.checkout').classList.remove('hidden');
+    //cartSumPrice.innerHTML = '$' + SumPrice();
+  } else {
+    //document.querySelector('.checkout').classList.add('hidden');
+    parentElement.innerHTML = '<h4 class="empty">Tu carrito esta vacio</h4>';
+    //cartSumPrice.innerHTML = '';
+  }
+};
+/// FUNCION PARA AGREGAR PRODUCTOS AL ARRAY SUMANDO LA CANTIDAD SI ES QUE YA ESTA AGREGADO
+function updateProductsInCart(product) {
+  // 2
+  for (let i = 0; i < productsInCart.length; i++) {
+    if (productsInCart[i].id == product.id) {
+      productsInCart[i].count += 1;
+      productsInCart[i].price =
+        productsInCart[i].basePrice * productsInCart[i].count;
+      return;
+    }
+  }
+  productsInCart.push(product);
+  console.log(productsInCart);
 }
 
-function updateProductsInCart(product) { // 2
-	for (let i = 0; i < productsInCart.length; i++) {
-		if (productsInCart[i].id == product.id) {
-			productsInCart[i].count += 1;
-			productsInCart[i].price = productsInCart[i].basePrice * productsInCart[i].count;
-			return;
-		}
-	}
-	productsInCart.push(product);
+/// RECORRO TODOS LOS PRODUCTOS Y EJECUTA LAS FUNCIONES EN CADA CLIC;
+function agregar(productos) {
+  for (const producto of productos) {
+    let botonPedir = document.getElementById(producto.id);
+    botonPedir.addEventListener("click", () => {
+      updateProductsInCart(producto);
+      updateShoppingCartHTML();
+    });
+  }
 }
+console.log(productsInCart);
 
-products.forEach(item => {   // 1
-	item.addEventListener('click', (e) => {
-		if (e.target.classList.contains('addToCart')) {
-			const productID = e.target.dataset.productId;
-			const productName = item.querySelector('.productName').innerHTML;
-			const productPrice = item.querySelector('.priceValue').innerHTML;
-			const productImage = item.querySelector('img').src;
-			let product = {
-				name: productName,
-				image: productImage,
-				id: productID,
-				count: 1,
-				price: +productPrice,
-				basePrice: +productPrice,
-			}
-			updateProductsInCart(product);
-			updateShoppingCartHTML();
-		}
-	});
+agregar(listaProductos);
+
+/// EVENTO PARA CONTROLAR LOS CLICKS EN BOTONES DE + Y - TANTO SEA PARA SUMAR CANTIDAD DE PRODUCTOS COMP PARA RESTAR
+parentElement.addEventListener("click", (e) => {
+  // Last
+  const plusBtn = e.target.classList.contains("button-plus");
+  const minusBtn = e.target.classList.contains("button-minus");
+  if (plusBtn || minusBtn) {
+    for (let i = 0; i < productsInCart.length; i++) {
+      if (productsInCart[i].id == e.target.dataset.id) {
+        if (plusBtn) {
+          productsInCart[i].count += 1;
+        } else if (minusBtn) {
+          productsInCart[i].count -= 1;
+        }
+        productsInCart[i].price =
+          productsInCart[i].basePrice * productsInCart[i].count;
+      }
+      if (productsInCart[i].count <= 0) {
+        productsInCart.splice(i, 1);
+      }
+    }
+    updateShoppingCartHTML();
+  }
 });
-
-parentElement.addEventListener('click', (e) => { // Last
-	const isPlusButton = e.target.classList.contains('button-plus');
-	const isMinusButton = e.target.classList.contains('button-minus');
-	if (isPlusButton || isMinusButton) {
-		for (let i = 0; i < productsInCart.length; i++) {
-			if (productsInCart[i].id == e.target.dataset.id) {
-				if (isPlusButton) {
-					productsInCart[i].count += 1
-				}
-				else if (isMinusButton) {
-					productsInCart[i].count -= 1
-				}
-				productsInCart[i].price = productsInCart[i].basePrice * productsInCart[i].count;
-
-			}
-			if (productsInCart[i].count <= 0) {
-				productsInCart.splice(i, 1);
-			}
-		}
-		updateShoppingCartHTML();
-	}
-});
-
-updateShoppingCartHTML();
